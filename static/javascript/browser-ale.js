03605e34f1cc61d9462eec5b6f2facc5b619c2ff
alessandro <alessandro@MBPdiAlessandro.station>
2019-10-13 22:26:21 +0200

    Inserimento delle direzioni

--- static/javascript/browser-ale.js
+++ static/javascript/browser-ale.js
@@ -76,29 +76,11 @@ searchControl.on('results', function (data) {
 });
 
 // indications on maps
-/*   https://github.com/skedgo/tripkit-leaflet
-let options ={
-    "mapId" : "map",
-    "googleTile": true,
-    "floatPanel": false,
-    "tripgoApiKey": "ba321828182b6531d9f5dd03c5055f4b"
-}
-L.tripgoRouting.mapLayer.initialize(options);
- */
-// indications on maps
-var control = L.Routing.control(L.extend(window.lrmConfig, {
-
+L.Routing.control(L.extend(window.lrmConfig, {
     geocoder: L.Control.Geocoder.nominatim(),
     routeWhileDragging: true,
     reverseWaypoints: true,
     showAlternatives: true,
-    altLineOptions: {
-        styles: [
-            {color: 'black', opacity: 0.15, weight: 9},
-            {color: 'white', opacity: 0.8, weight: 6},
-            {color: 'blue', opacity: 0.5, weight: 2}
-        ]
-    }
 })).addTo(map);
 
 L.Routing.errorControl(control).addTo(map);
