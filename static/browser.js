9b32028038acccd9747c0edc960b8f7fed6750a8
enricomors <enrico.morselli@studio.unibo.it>
2019-10-13 13:47:28 +0200

    merge 13/10

--- static/browser.js
+++ static/browser.js
@@ -1,6 +1,6 @@
 /** Script per browser.html */
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3VzdGF6IiwiYSI6ImNrMWphcDk1MzB4aWwzbnBjb2N5NDZ0bG4ifQ.ijWf_bZClD4nTcL91sBueg';
 
-/** TODO: Settare la mappa, ottenere TOKEN per mandare richieste all'API di Mapbox */
 
 /** Mapbox token */
 /** pk.eyJ1Ijoic3VzdGF6IiwiYSI6ImNrMWphcDk1MzB4aWwzbnBjb2N5NDZ0bG4ifQ.ijWf_bZClD4nTcL91sBueg */
