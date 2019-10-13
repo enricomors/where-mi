7dc261890978e424de2652615afe9b2f335eee6e
enricomors <enrico.morselli@studio.unibo.it>
2019-10-13 18:23:45 +0200

    nuovo file browser.js

--- static/javascript/browser.js
+++ static/javascript/browser.js
@@ -1,212 +1,188 @@
-var map = L.map('map').fitWorld();
+import { generateKeyPair } from "crypto";
 
-var markerPosizioneAttuale;
-var circlePosizioneAttuale;
-var markerSearch;
-var circleSearch;
+/** Mapbox token */
+const leaflet_access_token = 'pk.eyJ1Ijoic3VzdGF6IiwiYSI6ImNrMWphcDk1MzB4aWwzbnBjb2N5NDZ0bG4ifQ.ijWf_bZClD4nTcL91sBueg';
+/** Coordinate di default */
+const default_coords = [44.4940258,11.340965];
 
-var markerDraggable;
-var circleDraggable;
-
-var greenIcon = new L.Icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+/** Icone per i marker */
+var blueIcon = L.icon({
+    iconUrl: '/blue-icon.png',
+	iconSize: [25, 40],
+	iconAnchor: [12, 40],
+	popupAnchor: [1, -35],
+});
+var redIcon = L.icon({
+    iconUrl: '/red-icon.png',
+	iconSize: [25, 40],
+	iconAnchor: [12, 40],
+	popupAnchor: [1, -35],
 });
 
+/** Mostra nuovo marker sulla mappa */
+var newPoint = L.marker();
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3VzdGF6IiwiYSI6ImNrMWphcDk1MzB4aWwzbnBjb2N5NDZ0bG4ifQ.ijWf_bZClD4nTcL91sBueg', {
-    maxZoom: 18,
-    id: 'mapbox.streets'
-}).addTo(map);
+var currPosition;
+var selectedPoint;
+var tappa;
+var myPosition;
+var routingControl = null;
 
-// add location via browser geolocation
+/** Clip di youtube */
+var idYT = [];
+var datiVideo = {};
 
-function displayLocation(position) {
-   // console.log('position', position);
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    //L.marker([lat, lng]).addTo(map);
-    //console.log('{longitude:' + lng + ', latitude:' + lat + '}');
-    map.setView([lat, lng], 18);
-    markerPosizioneAttuale=L.marker([lat, lng]).addTo(map)
-        .bindPopup(`<h5><strong>YOU ARE HERE!</strong></h5>`)
-        .openPopup();
+/** Quando il DOM è pronto ad eseguire il codice js */
+$(document).ready(() => {
+    // Inizializza la mappa
+    mapInit(default_coords);
+    // mostra le card per le clip
+    showCards();
+});
 
-    circlePosizioneAttuale = L.circle([lat, lng], {
-        color: 'red',
-        fillColor: '#f06',
-        fillOpacity: 0.3,
-        radius: 100
+/** Inizializza la mappa leaflet */
+function mapInit(coords) {
+    /** Crea nuova mappa Setta le coordinate di default e il livello di zoom */
+    map = L.map('map').map.setView(coords, 14);
+    /** Setta il tileLayer */
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+ 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
+ 		maxZoom: 22,
+ 		id: 'mapbox.streets',
+ 		accessToken: leaflet_access_token
     }).addTo(map);
+    /** Richiesta location */
+    getLocation();
+};
+
+/** Aggiorna la mappa con le coordinate ricevute dal browser */
+function updateMap(coords) {
+    if (currPosition) {
+        currPosition.remove();
+    }
+    lat = coords.latitude;
+    lng = coords.longitude;
+    /** Setta la mappa sulle nuove coordinate */
+    map.setView([lat, lng], 14);
+    /** Marker della posizone corrente */
+    currPosition = L.marker([lat, lng], { draggable: 'true' });
+    currPosition.setIcon(redIcon);
+    /** Crea il popup per il marker */
+    currPosition.bindPopup(
+        `<div style="text-align: center;">
+		<h6 class="text-uppercase" style="margin-top: 2%;">You are here</h6>
+		<hr align="center">If location is incorrect, drag the marker</a>
+        </div>`).openPopup();
+    /** Aggiunge il marker alla mappa */
+    currPosition.addTo(map);
+    currPosition.on('click', onMarkerClick);
+};
+
+/** Gestisce l'evento di click su marker esistente */
+function onMarkerClick(marker) {
+    newPoint.remove();
+    if (selectedPoint) {
+        selectedPoint.setIcon(blueIcon);
+    }
+    selectedPoint = marker.target;
+    selectedPoint.setIcon(redIcon);
 }
 
-navigator.geolocation.getCurrentPosition(displayLocation);
+/** Ottiene la posizione attuale */
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(
+        pos => updateMap(pos.coords)
+    );
+};
 
-var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+function showCards() {
 
-var results = L.layerGroup().addTo(map);
-
-searchControl.on('results', function (data) {
-    results.clearLayers();
-    for (var i = data.results.length - 1; i >= 0; i--) {
-
-         markerSearch = results.addLayer(L.marker(data.results[i].latlng));
-         circleSearch = results.addLayer(L.circle(data.results[i].latlng, {
-            color: 'red',
-            fillColor: '#f06',
-            fillOpacity: 0.3,
-            radius: 100
-        }));
-
-            map.removeLayer(markerPosizioneAttuale);
-            map.removeLayer(circlePosizioneAttuale);
-        if (markerDraggable) {
-            map.removeLayer(markerDraggable);
-        }
-        if (circleDraggable) {
-            map.removeLayer(circleDraggable);
-        }
-
-    }
-});
-
-// indications on maps
-/*   https://github.com/skedgo/tripkit-leaflet
-let options ={
-    "mapId" : "map",
-    "googleTile": true,
-    "floatPanel": false,
-    "tripgoApiKey": "ba321828182b6531d9f5dd03c5055f4b"
-}
-L.tripgoRouting.mapLayer.initialize(options);
-
- */
-// indications on maps
-var control = L.Routing.control(L.extend(window.lrmConfig, {
-
-    geocoder: L.Control.Geocoder.nominatim(),
-    routeWhileDragging: true,
-    reverseWaypoints: true,
-    showAlternatives: true,
-    altLineOptions: {
-        styles: [
-            {color: 'black', opacity: 0.15, weight: 9},
-            {color: 'white', opacity: 0.8, weight: 6},
-            {color: 'blue', opacity: 0.5, weight: 2}
-        ]
-    }
-})).addTo(map);
-
-L.Routing.errorControl(control).addTo(map);
-
-/*
-function createButton(label, container) {
-    var btn = L.DomUtil.create('button', '', container);
-    btn.setAttribute('type', 'button');
-    btn.innerHTML = label;
-    return btn;
-}
-
-map.on('click', function(e) {
-    var container = L.DomUtil.create('div'),
-        startBtn = createButton('Start from this location', container),
-        destBtn = createButton('Go to this location', container);
-
-    L.popup()
-        .setContent(container)
-        .setLatLng(e.latlng)
-        .openOn(map);
-});
-
-L.DomEvent.on(startBtn, 'click', function() {
-    control.spliceWaypoints(0, 1, e.latlng);
-    map.closePopup();
-});
-
-L.DomEvent.on(destBtn, 'click', function() {
-    control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
-    map.closePopup();
-});
-/*
-L.popup()
-    .setContent(container)
-    .setLatLng(e.latlng)
-    .openOn(map);
-*/
-
-
-// set the popup information: latlng and address
-function addPopup (marker) {
-    // OSM Nomitatim documentation: http://wiki.openstreetmap.org/wiki/Nominatim
-    var jsonQuery = "http://nominatim.openstreetmap.org/reverse?format=json&lat=" + marker.getLatLng().lat + "&lon=" + marker.getLatLng().lng + "&zoom=18&addressdetails=1";
-
-    $.getJSON(jsonQuery).done( function (result_data) {
-        console.log(result_data);
-
-        var road;
-
-        if(result_data.address.road) {
-            road = result_data.address.road;
-        }
-        else if (result_data.address.pedestrian) {
-            road = result_data.address.pedestrian;
-        }
-        else {
-            road = "No defined";
-        }
-        var olc= OpenLocationCode.encode(marker.getLatLng().lat, marker.getLatLng().lng, 10);
-
-        var popup_text = 	"<b>Olc:</b> "+ olc  +
-            "</br><b>Road:</b> " + road + ", " + result_data.address.house_number +
-            "</br><b>City:</b> " + result_data.address.city +
-            "</br><b>Postal Code:</b> " + result_data.address.postcode;
-
-        marker.bindPopup(popup_text).openPopup();
-
-        map.removeLayer(markerPosizioneAttuale);
-        map.removeLayer(circlePosizioneAttuale);
-        if(markerSearch) {
-            map.removeLayer(markerSearch);
-            map.removeLayer(circleSearch);
-        }
-
-
-    });
-
-}
-
-
-// add new marker on click
-map.on('dblclick', function(e) {
-
-    // removes old marker
-    if (markerDraggable) {
-        map.removeLayer(markerDraggable);
-    }
-    if (circleDraggable) {
-        map.removeLayer(circleDraggable);
-    }
-
-    // add draggable marker
-    circleDraggable = L.circle([e.latlng.lat, e.latlng.lng], {
-        color: 'red',
-        fillColor: '#f06',
-        fillOpacity: 0.3,
-        radius: 100
-    }).addTo(map);
-    markerDraggable = L.marker([e.latlng.lat, e.latlng.lng], {draggable: true})
-        .addTo(map)
-        .on('dragend', function(e) {
-
-            // add popup information on dragged marker
-            addPopup(markerDraggable);
+    gapi.client.load('youtube', 'v3', () => {
+        // richiesta ricerca clip
+        var req = gapi.client.youtube.search.list({
+            part: 'snippet',
+            type: 'video',
+            q: '8FPHF9Q5+J4',
+            maxResults: 50,
+            order: 'title'
         });
+        // esegue la richiesta
+        req.execute((resp) => {
+            console.log(resp);
+            // scorre le risorse contenute nella risposta
+            resp.result.items.forEach((item) => {
+                // estrae i dati del video
+                let name = item.snippet.title.split(":")[0];
+                let metaDati = item.snippet.description.split("#")[0];
+                let description = item.snippet.description.split("#")[1];
+                let idVideo = item.id.videoId;
+                // inserisce id del video in idYT
+                idYT.push(idVideo);
+                // estrare i uno per uno i metadati dalla stringa
+                let olc = metaDati.split(":")[0];
+                let purpose = metadati.split(":")[1];
+            	let language = metadati.split(":")[2];
+                let category = metadati.split(":")[3];
+                let audience = metadati.split(":")[4];
+                let detail = metadati.split(":")[5];
+                // ricava le coordinate della clip dall'olc
+                let coords = OpenLocationCode.decode(olc);
+                let dati = {
+                    "purpose": purpose,
+                    "language": language,
+                    "category": category,
+                    "audience": audience,
+                    "detail": detail
+                };
+                datiVideo[idVideo] = dati;
+                // crea popup per il marker della clip 
+                let popup = 
+                `<div id="${idVideo}popup" style="text-align: center;">
+                <h5 class="text-uppercase" style="margin-top: 2%;">${name}</h5>
+                <hr align="center">
+                <a id="${idVideo}link" class="btn" style="color: #fed136;" href="#${idVideo}card">Vai alla clip!</a>
+                </div>`;
+                // crea marker nelle posizioni delle clips
+                var marker = L.marker([coords.latitudeCenter, coords.longitudeCenter], { myCustomId: idVideo + "map" });
+                // aggiunge il marker alla mappa
+                marker.bindPopup(popup).addTo(map);
+                // click listener per il marker
+                marker.on('click', routing);
+            });
+        });
+    });
+    
+};
 
-    // add popup information on new marker
-    addPopup(markerDraggable);
-});
+/** Funzione di navigazione */
+function routing() {
+    
+}
 
+
+/**
+ * {
+  "kind": "youtube#searchResult",
+  "etag": etag,
+  "id": {
+    "kind": string,
+    "videoId": string,
+    "channelId": string,
+    "playlistId": string
+  },
+  "snippet": {
+    "publishedAt": datetime,
+    "channelId": string,
+    "title": string,
+    "description": string,
+    "thumbnails": {
+      (key): {
+        "url": string,
+        "width": unsigned integer,
+        "height": unsigned integer
+      }
+    },
+    "channelTitle": string,
+    "liveBroadcastContent": string
+  }
+ */
