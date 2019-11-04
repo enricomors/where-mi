eb47505541c57f9e31a90146edb86343ff5e11db
enricomors <enrico.morselli@studio.unibo.it>
2019-11-04 11:11:39 +0100

    corretto un errore mio nel browser

--- static/javascript/browser.js
+++ static/javascript/browser.js
@@ -76,7 +76,7 @@ if(!navigator.geolocation){
 /** Mostra sulla mappa la posizione ricevuta dal browser */
 function displayLocation(position) {
     // apre la mappa sulla posizione ricevuta dal browser
-    map.setView([lat, lng], 18);
+    map.setView([position.coords.latitude, position.coords.longitude], 18);
     // aggiorna marker
     updateMarker(position.coords.latitude, position.coords.longitude);
     //aggiorna posizione
@@ -153,158 +153,168 @@ function loadYTVideos() {
         // salva la risposta nell'array results
         var results = resp.result.items;
         console.log(results);
-        // scorre le risorse contenute nella risposta
-        for (var i = 0; i < results.length; i++) {
-            let name;
-            // titolo del video
-            if (results[i].snippet.title.indexOf(':') != -1) {
-                name = results[i].snippet.title.split(':')[0];
-            } else {
-                name = results[i].snippet.title;
-            }
-            // estrae i dati dell'i-esimo video
-            let metaDati = results[i].snippet.description.split("#")[0];
-            let description = results[i].snippet.description.split("#")[1];
-            let idVideo = results[i].id.videoId;
-            console.log(idVideo);
-            let idPrev;
-            let idNext;
-            // ricava id del video precedente
-            if (i == 0) {
-                // per il primo video, l'id precedente è quello dell'ultimo video
-                idPrev = results[results.length - 1].id.videoId;
-            } else {
-                idPrev = results[i - 1].id.videoId;
-            }
-            // ricava id del video successivo
-            if (i == (results.length - 1)) {
-                // per ultimo video, l'id successivo è quello del primo video
-                idNext = results[0].id.videoId;
-            } else {
-                idNext = results[i + 1].id.videoId;
-            }
-            // inserisce id del video in idYT
-            idYT.push(idVideo);
-            // estrare i uno per uno i metadati dalla stringa
-            let olc = metaDati.split(":")[0];
-            // variabile per le coordinate della clip
-            let coords;
-            try {
-                // ricava le coordinate della clip dall'olc
-                coords = OpenLocationCode.decode(olc);
-            } catch (IllegalArgumentException) {
-                coords = OpenLocationCode.decode(currentOlc);
-            }
-            let purpose = metaDati.split(":")[1];
-            let language = metaDati.split(":")[2];
-            let category = metaDati.split(":")[3];
-            let audience = metaDati.split(":")[4];
-            let detail = metaDati.split(":")[5];
-            let descrizione= metaDati.split(":")[6];
-            // dati della clip
-            let dati = {
-                "purpose": purpose,
-                "language": language,
-                "category": category,
-                "audience": audience,
-                "detail": detail,
-                "descrizione": descrizione
-            };
-            datiVideo[idVideo] = dati;
-            // crea popup per il marker della clip
-            let popup =
-            `<div id="${idVideo}popup" style="text-align: center;">
-            <h5 class="text-uppercase" style="margin-top: 2%;">${name}</h5>
-            <hr align="center">
-            <a id="${idVideo}link" class="btn" style="color: #04af73;" href="#${idVideo}card">Vai alla clip!</a>
-            </div>`;
-            // crea marker nelle posizioni delle clips
-            var marker = new L.marker([coords.latitudeCenter, coords.longitudeCenter], { myCustomId: idVideo + "map" })
-                .bindPopup(popup).addTo(map).on('click', routing);
-            // aggiunge le card delle clip nella sezione #clips
-            $('#clips').append(
-                `<!-- Start: Clip Cards -->
-                <article id="${idVideo}card" class="col-sm-4 col-md-4 col-lg-3" style="margin-bottom: 2%;">
-                <div class="card cards-shadown cards-hover" style="height: 35rem;">
+        // se la ricerca produce risultati
+        if (results.length != 0) {
+            // scorre le risorse contenute nella risposta
+            for (var i = 0; i < results.length; i++) {
+                let name;
+                // titolo del video
+                if (results[i].snippet.title.indexOf(':') != -1) {
+                    name = results[i].snippet.title.split(':')[0];
+                } else {
+                    name = results[i].snippet.title;
+                }
+                // estrae i dati dell'i-esimo video
+                let metaDati = results[i].snippet.description.split("#")[0];
+                let description = results[i].snippet.description.split("#")[1];
+                let idVideo = results[i].id.videoId;
+                console.log(idVideo);
+                let idPrev;
+                let idNext;
+                // ricava id del video precedente
+                if (i == 0) {
+                    // per il primo video, l'id precedente è quello dell'ultimo video
+                    idPrev = results[results.length - 1].id.videoId;
+                } else {
+                    idPrev = results[i - 1].id.videoId;
+                }
+                // ricava id del video successivo
+                if (i == (results.length - 1)) {
+                    // per ultimo video, l'id successivo è quello del primo video
+                    idNext = results[0].id.videoId;
+                } else {
+                    idNext = results[i + 1].id.videoId;
+                }
+                // inserisce id del video in idYT
+                idYT.push(idVideo);
+                // estrare i uno per uno i metadati dalla stringa
+                let olc = metaDati.split(":")[0];
+                // variabile per le coordinate della clip
+                let coords;
+                try {
+                    // ricava le coordinate della clip dall'olc
+                    coords = OpenLocationCode.decode(olc);
+                } catch (IllegalArgumentException) {
+                    coords = OpenLocationCode.decode(currentOlc);
+                }
+                let purpose = metaDati.split(":")[1];
+                let language = metaDati.split(":")[2];
+                let category = metaDati.split(":")[3];
+                let audience = metaDati.split(":")[4];
+                let detail = metaDati.split(":")[5];
+                let descrizione = metaDati.split(":")[6];
+                // dati della clip
+                let dati = {
+                    "purpose": purpose,
+                    "language": language,
+                    "category": category,
+                    "audience": audience,
+                    "detail": detail,
+                    "descrizione": descrizione
+                };
+                datiVideo[idVideo] = dati;
+                // crea popup per il marker della clip
+                let popup =
+                `<div id="${idVideo}popup" style="text-align: center;">
+                <h5 class="text-uppercase" style="margin-top: 2%;">${name}</h5>
+                <hr align="center">
+                <a id="${idVideo}link" class="btn" style="color: #04af73;" href="#${idVideo}card">Vai alla clip!</a>
+                </div>`;
+                // crea marker nelle posizioni delle clips
+                var marker = new L.marker([coords.latitudeCenter, coords.longitudeCenter], { myCustomId: idVideo + "map" })
+                    .bindPopup(popup).addTo(map).on('click', routing);
+                // aggiunge le card delle clip nella sezione #clips
+                $('#clips').append(
+                    `<!-- Start: Clip Cards -->
+                    <article id="${idVideo}card" class="col-sm-4 col-md-4 col-lg-3" style="margin-bottom: 2%;">
+                    <div class="card cards-shadown cards-hover" style="height: 35rem;">
 
-                <!-- CARD HEADER-->
-                <div id="${idVideo}header" class="card-header text-left" style="background-color: #04af73;width: 100%;height: 100%;">
-                <span class="space"><a id="${idVideo}map" class="btn" href="#map" style="color: white;">Vedi sulla mappa</i></a></span>
-                <div class="cardheader-text" style="color: white;">
-                <h4 id="heading-card" style="font-size: 26px;margin-top: 7%;">${name}</h4>
-                <p id="cardheader-subtext"><i>Purpose:&nbsp</i><span class="text-uppercase"> ${purpose}</span></p>
-                </div>
-                </div>
+                    <!-- CARD HEADER-->
+                    <div id="${idVideo}header" class="card-header text-left" style="background-color: #04af73;width: 100%;height: 100%;">
+                    <span class="space"><a id="${idVideo}map" class="btn" href="#map" style="color: white;">Vedi sulla mappa</i></a></span>
+                    <div class="cardheader-text" style="color: white;">
+                    <h4 id="heading-card" style="font-size: 26px;margin-top: 7%;">${name}</h4>
+                    <p id="cardheader-subtext"><i>Purpose:&nbsp</i><span class="text-uppercase"> ${purpose}</span></p>
+                    </div>
+                    </div>
 
-                <!-- CARD BODY-->
-                <div class="card-body" style="color:black;">
-                <ul class="list-group text-left">
-                <li class="list-group-item"><span><i><b>Language:&nbsp</b></i> ${language}</span></li>
-                <li class="list-group-item"><span><i><b>Category:&nbsp</b></i> ${category}</span></li>
-                <li class="list-group-item"><span><i><b>Audience:&nbsp</b></i> ${audience}</span></li>
-                <li class="list-group-item" style="height: 5rem; overflow: auto;"><span><i><b>Description:&nbsp</b></i> ${descrizione}</span></li>
-                </ul>
-                </div>
+                    <!-- CARD BODY-->
+                    <div class="card-body" style="color:black;">
+                    <ul class="list-group text-left">
+                    <li class="list-group-item"><span><i><b>Language:&nbsp</b></i> ${language}</span></li>
+                    <li class="list-group-item"><span><i><b>Category:&nbsp</b></i> ${category}</span></li>
+                    <li class="list-group-item"><span><i><b>Audience:&nbsp</b></i> ${audience}</span></li>
+                    <li class="list-group-item" style="height: 5rem; overflow: auto;"><span><i><b>Description:&nbsp</b></i> ${descrizione}</span></li>
+                    </ul>
+                    </div>
 
-                <!-- CARD FOOTER-->
-                <div class="card-footer text-center">
-                <button id="${idPrev}" class="btn-previous">
-                <i class="fa fa-backward" id="previous" style="font-size: 30px;"></i>
-                </button>
-                <button id="${idVideo}" class="btn-play">
-                <i class="fa fa-play-circle" style="font-size: 30px;padding-right: 5%;"></i>
-                </button>
-                <button class="btn-pause">
-                <i class="fa fa-pause-circle" style="font-size: 30px;"></i>
-                </button>
-                <button id="${idNext}" class="btn-next">
-                <i class="fa fa-forward" id="next" style="font-size: 30px;"></i>
-                </button>
-                </div>
-                </div>
-                <!-- End: Clip Cards -->
+                    <!-- CARD FOOTER-->
+                    <div class="card-footer text-center">
+                    <button id="${idPrev}" class="btn-previous">
+                    <i class="fa fa-backward" id="previous" style="font-size: 30px;"></i>
+                    </button>
+                    <button id="${idVideo}" class="btn-play">
+                    <i class="fa fa-play-circle" style="font-size: 30px;padding-right: 5%;"></i>
+                    </button>
+                    <button class="btn-pause">
+                    <i class="fa fa-pause-circle" style="font-size: 30px;"></i>
+                    </button>
+                    <button id="${idNext}" class="btn-next">
+                    <i class="fa fa-forward" id="next" style="font-size: 30px;"></i>
+                    </button>
+                    </div>
+                    </div>
+                    <!-- End: Clip Cards -->
 
-                <script>
-                $("#${idVideo}map").click(function(){
-                    $.each(map._layers, function(i, item){
-                        if(this.options.myCustomId == "${idVideo}map"){
-                            this.openPopup();
-                            map.flyTo(this._latlng)
-                        }
+                    <script>
+                    $("#${idVideo}map").click(function(){
+                        $.each(map._layers, function(i, item){
+                            if(this.options.myCustomId == "${idVideo}map"){
+                                this.openPopup();
+                                map.flyTo(this._latlng)
+                            }
+                        });
                     });
-                });
 
-                $(".btn-play").click(function(){
-                    player.loadVideoById(this.id);
-                    console.log(this.id);
-                    player.playVideo();
-                    // aggiornamento UI
-                    console.log("#"+this.id+"header");
-                    $("#"+this.id+"header").css("background-color","#006633");
-                });
+                    $(".btn-play").click(function(){
+                        player.loadVideoById(this.id);
+                        console.log(this.id);
+                        player.playVideo();
+                        // aggiornamento UI
+                        console.log("#"+this.id+"header");
+                        $("#"+this.id+"header").css("background-color","#006633");
+                    });
 
-                $(".btn-pause").click(function(){
-                    player.pauseVideo();
-                });
+                    $(".btn-pause").click(function(){
+                        player.pauseVideo();
+                    });
 
-                $(".btn-next").click(function(){
-                    player.clearVideo();
-                    player.loadVideoById(this.id);
-                    player.playVideo();
-                    // aggiornamento UI
-                    $("#"+this.id+"header").css("background-color","#006633");
-                });
+                    $(".btn-next").click(function(){
+                        player.clearVideo();
+                        player.loadVideoById(this.id);
+                        player.playVideo();
+                        // aggiornamento UI
+                        $("#"+this.id+"header").css("background-color","#006633");
+                    });
 
-                $(".btn-previous").click(function(){
-                    player.clearVideo();
-                    player.loadVideoById(this.id);
-                    player.playVideo();
-                    // aggiornamento UI
-                    $("#"+this.id+"header").css("background-color","#006633");
-                });
+                    $(".btn-previous").click(function(){
+                        player.clearVideo();
+                        player.loadVideoById(this.id);
+                        player.playVideo();
+                        // aggiornamento UI
+                        $("#"+this.id+"header").css("background-color","#006633");
+                    });
 
-                </script>
-                </article>`
+                    </script>
+                    </article>`
+                );
+            }
+        } else {
+            alert("Nella zona non sono presenti clip");
+            $('#clips').append(
+                `<div class="text-center">
+                    <p>Non sono presenti clip nella zona</p>
+                </div>`
             );
         }
     });
