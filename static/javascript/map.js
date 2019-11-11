2fde98cf05534f49e1cb7c878a8b68f1090bdec3
Enrico Morselli <enrico.morselli@studio.unibo.it>
2019-11-11 15:03:30 +0100

    aggiunti png marker e shadow

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -11,8 +11,8 @@ var routingControl = null;
 
 /** Marker verde */
 var greenIcon = new L.Icon({
-  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconUrl: './static/images/marker-icon-green.png',
+  shadowUrl: './static/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
